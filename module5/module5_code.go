fee1faa0102ec77f51817ed8176a0858611fcf7b
Gianstefano Monni <gmonni@salesforce.com>
2021-01-14 17:00:05 +0100

    easyfix

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net/http"
 )
-// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 
+// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
